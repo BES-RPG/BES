7d540782c5ba6e16293f0dcde88475d9b5f42751
Slusheyo <68085490+Slusheyo@users.noreply.github.com>
2021-02-09 19:32:17 -0500

    more stuff to roles

--- src/js/roles/roles.js
+++ src/js/roles/roles.js
@@ -1,9 +1,40 @@
-class Roles {
-    constructor() {
+// Must make class and methods for the roles
+// ex. Attack, SpecialAttack, SpecialSkill
+export class Roles {
+    constructor(name, stats, skills) {
+        this.name = name;
+        this.stats = {
+            speed: 100,
+            buff: 100,
+            health: 100,
+            iq: 100
+        };
+        this.skills = skills;
+    }
+    // The Abilities that the roles can have (heal, speedboost, damageboost, moredamage)
+    healAbility() {
+        if (this.stats.health <= 100) {
+            this.stats.health += 50;
+        } else if (this.stats.health >= 100) {
+            // Must make it so that the health returns to 100, or the original health +50 as long as it is under or equal to 100
+            this.stats.health -= 50;
+        }
+    }
+
+    speedBoostAbility() {
 
     }
 
-    attack() {
+    damageBoostAbility() {
+
+    }
+
+    criticalAbility() {
+
+    }
+
+    gotHit() {
 
     }
 }
+
